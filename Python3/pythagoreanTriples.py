4c0b003c933797a2d14966e90b3988ed645a1ef6
spacecoffin <reed.rosenberg@gmail.com>
2014-07-03 13:39:32 -0400

    Implemented co-prime test for pT
    Used gcd function from fractions module. Two numbers are co-prime if
    gcd(a, B) == 1.

--- Python3/pythagoreanTriples.py
+++ Python3/pythagoreanTriples.py
@@ -1,23 +1,4 @@
-from math import sqrt
-from math import floor
-
-def isPrime(num):
-    if num > 1:
-        if num == 2 or num == 3:
-            return True
-        if num % 2 == 0:
-            return False
-        for i in range(3, num, 2):
-            if num % i == 0:
-                return False
-        return True
-    return False
-
-def nextPrime(num):
-    while True:
-        if isPrime(num):
-            return num
-        num += 1
+from fractions import gcd
 
 def PythTripleGen():
     total = 3
@@ -28,14 +9,8 @@ def PythTripleGen():
             total += 2 # skip sums of p & q that are even
             p = 1
             q = total - 1
-        # 1) p&q must be mutually coprime
-        # 2) to find if mutually coprime, take sqrt of q,
-        #   then find first prime factor that is less than p
-        #   -or- check each prime factor that is less than p
-        primediv = floor(sqrt(q))
-        
-            
-        yield q**2 - p**2, 2*q*p, p**2 + q**2
+        if gcd(p, q) == 1: # check that p & q are coprime
+            yield q**2 - p**2, 2*q*p, p**2 + q**2
         p += 1
         q = total - p
 
