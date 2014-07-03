10122ffbc141c6b5c5e75c36afe1672c0121f583
spacecoffin <reed.rosenberg@gmail.com>
2014-07-03 13:15:47 -0400

    updated to pythTrip & created triFact
    pT: Made notes on how to avoid pythTriples that are not mutually
    co-prime and whose p&q summed is odd.

--- Python3/pythagoreanTriples.py
+++ Python3/pythagoreanTriples.py
@@ -7,11 +7,13 @@ def PythTripleGen():
             total += 2
             p = 1
             q = total - 1
+        # 1) p&q must be mutually coprime
+        # 2) to find if mutually coprime, take sqrt of q,
+        #   then find first prime factor that is less than p
+        #   -or- check each prime factor that is less than p
         yield q**2 - p**2, 2*q*p, p**2 + q**2
         p += 1
         q = total - p
-    
-    
 
 if __name__ == '__main__':
     query = "T to generate the next triple, Q to quit: "
